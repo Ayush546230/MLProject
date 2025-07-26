f945fcdee6dfbcd09e1406b61518dbe3c5376a20
Ayush546230 <122547470+Ayush546230@users.noreply.github.com>
2025-07-26 11:13:37 +0530

    Project structure ready, venv ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ayush Srivastava",
+    author_email="ayushsrivastava18623@gmail.com",
+    packages=find_packages()
+)
